139319d13d84bc6669e9eb3249a30197cc97ea99
Maluma Tolley maluma_0014@yahoo.com <maluma_0014@yahoo.com>
2025-06-03 11:08:45 +0100

    Models Controller Review/Bookings

--- app/models/desk.rb
+++ app/models/desk.rb
@@ -1,4 +1,5 @@
 class Desk < ApplicationRecord
   belongs_to :user
+  has_many :reviews, dependent: :destroy
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_06_02_152221) do
+ActiveRecord::Schema[7.1].define(version: 2025_06_03_095635) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.date "start_date"
+    t.date "end_date"
+    t.bigint "user_id", null: false
+    t.bigint "desk_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["desk_id"], name: "index_bookings_on_desk_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "desks", force: :cascade do |t|
     t.text "description"
@@ -25,6 +36,13 @@ ActiveRecord::Schema[7.1].define(version: 2025_06_02_152221) do
     t.string "title"
     t.integer "price"
     t.index ["user_id"], name: "index_desks_on_user_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.text "content"
+    t.integer "rating"
   end
 
   create_table "users", force: :cascade do |t|
@@ -41,5 +59,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_06_02_152221) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "desks"
+  add_foreign_key "bookings", "users"
   add_foreign_key "desks", "users"
 end
