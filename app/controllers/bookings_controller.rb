8ce07e305639e7f031b99cf09acd915a9e19bf66
laura salesse <salesse.laura@gmail.com>
2025-06-05 15:30:08 +0200

    done with carrousel and alerts when creating  a new booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -22,8 +22,8 @@ class BookingsController < ApplicationController
     if @booking.save
       nights = (@booking.end_date - @booking.start_date).to_i
       desk_title = @booking.desk.title
-      flash[notice] = "you just booked the flat #{desk_title} for #{nights} night#{'s' if nights > 1}!"
-      redirect_to renter_bookings_path, notice: "Booking created successfully!"
+      flash[:notice] = "you just booked the flat #{desk_title} for #{nights} night#{'s' if nights > 1}!"
+      redirect_to renter_bookings_path, notice: "you just booked the flat '#{desk_title}' for #{nights} night#{'s' if nights > 1}! See the booking below"
     else
       redirect_to desk_path(@desk), alert: "Could not create booking. Please select valid dates."
     end
